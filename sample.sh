9ed83cdccf99313d312dfed85e699c7d425d548c
yamunarathod <yamunarathod779@gmail.com>
2025-02-11 12:44:49 +0530

    new-one

--- sample.sh
+++ sample.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# Define the base path for the Stable Diffusion models
+# Define the base path for the models
 BASE_PATH="/home/runner/ComfyUI/models"
 
 # Create necessary directories if they don't exist
-mkdir -p "$BASE_PATH/checkpoints"
-mkdir -p "$BASE_PATH/detection/bbox"
-mkdir -p "$BASE_PATH/sam"
+mkdir -p "$BASE_PATH/facerestore_models"
+mkdir -p "$BASE_PATH/facedetection"
+mkdir -p "$BASE_PATH/insightface"
 
 # Download Stable Diffusion models
-curl -L -o "$BASE_PATH/checkpoints/albedobaseXL_v13.safetensors" "https://huggingface.co/frankjoshua/albedobaseXL_v13/resolve/5770b538c319a7445d628c96fe7e2d80713346b0/albedobaseXL_v13.safetensors"
+curl -L -o "$BASE_PATH/facerestore_models/codeformer-v0.1.0.pth" "https://huggingface.co/ziixzz/codeformer-v0.1.0.pth/resolve/main/codeformer-v0.1.0.pth"
 
-curl -L -o "$BASE_PATH/checkpoints/realisticVisionV20_v20.safetensors" "https://huggingface.co/ckpt/realistic-vision-v20/resolve/main/realisticVisionV20_v20.safetensors"
+curl -L -o "$BASE_PATH/facedetection/retinaface_resnet50.onnx" "https://huggingface.co/OWG/resnet-50/resolve/main/onnx/model.onnx"
 
 # Download YOLO detection model
-curl -L -o "$BASE_PATH/detection/bbox/face_yolov8m.pt" "https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/detection/bbox/face_yolov8m.pt"
-
-# Download SAM model
-curl -L -o "$BASE_PATH/sam/sam_vit_h_4b8939.pth" "https://huggingface.co/spaces/abhishek/StableSAM/resolve/main/sam_vit_h_4b8939.pth"
+curl -L -o "$BASE_PATH/insightface/inswapper_128.onnx" "https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx"
 
 # Completion message
 echo "Download complete! Files are stored in the appropriate folders."
