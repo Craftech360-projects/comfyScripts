e3c779f3992dde69cc6fd11bc29a9305b08bfec0
yamunarathod <yamunarathod779@gmail.com>
2025-02-11 13:32:15 +0530

    cehc

--- sample.sh
+++ sample.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Define the base path where models should be downloaded
+# Define the base path for models
 BASE_PATH="$HOME/ComfyUI/models"
 
-# Create necessary directories
+# Create necessary directories if they don't exist
+mkdir -p "$BASE_PATH/checkpoints"
 mkdir -p "$BASE_PATH/facerestore_models"
 mkdir -p "$BASE_PATH/facedetection"
 mkdir -p "$BASE_PATH/insightface"
@@ -15,6 +16,9 @@ curl -L -o "$BASE_PATH/facedetection/retinaface_resnet50.onnx" "https://huggingf
 
 # Download YOLO detection model
 curl -L -o "$BASE_PATH/insightface/inswapper_128.onnx" "https://huggingface.co/ezioruan/inswapper_128.onnx/resolve/main/inswapper_128.onnx"
+
+# Download ahxl_v1.safetensors model into checkpoints
+curl -L -o "$BASE_PATH/checkpoints/ahxl_v1.safetensors" "https://huggingface.co/nahuiyang/custom/resolve/88ce5013c565938aa33daa9cf4ce7cba446a85e2/ahxl_v1.safetensors"
 
 # Completion message
 echo "Download complete! Files are stored in $BASE_PATH."
